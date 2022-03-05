0970280ffdffa5f1edfd1fdf44e853d734c8250b
Sher Singh <75926414+sher88@users.noreply.github.com>
2022-03-05 19:46:16 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA-lHd0TzM_gSfl6qnhGvNDjs-_1jZ4yv8",
+    authDomain: "whats-app-clone-b2f81.firebaseapp.com",
+    projectId: "whats-app-clone-b2f81",
+    storageBucket: "whats-app-clone-b2f81.appspot.com",
+    messagingSenderId: "602435550103",
+    appId: "1:602435550103:web:a22d8e4a799a84565d62d2",
+    measurementId: "G-37NF1DFVRH"
+};
